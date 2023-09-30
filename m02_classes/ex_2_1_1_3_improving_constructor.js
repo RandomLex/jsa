9eb34926d6aa9557bcac4529490cfd127837eb05
Alexej Barzykin <alexej.barzykin@gmail.com>
2023-10-01 00:17:06 +0400

    * better constructor

---
+++ m02_classes/ex_2_1_1_3_improving_constructor.js
@@ -0,0 +1,23 @@
+let Vehicle = function(id, latitude, longitude){
+
+  this.setPosition = function(latitude, longitude) {
+    this.time = Date.now();
+    this.longitude = longitude;
+    this.latitude = latitude;
+  };
+
+  this.id = id;
+  this.status = "unavailable";
+// A bit better way to do this:
+  this.setPosition(latitude, longitude);
+// instead of this:
+// this.time = Date.now();
+// this.latitude = latitude;
+// this.longitude = longitude;
+
+};
+
+let vehicle1 = new Vehicle("AL1024", 59.358615, 17.947589);
+vehicle1.setPosition(59.367647, 18.213451);
+console.log(vehicle1);
+
