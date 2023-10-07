ef4de186535947d32a02d3a61229ca5dc0455f71
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2023-10-07 23:17:21 +0400

    either numbers casting for abc variable by +abc or Number(abc) gives number as a type

---
+++ m03_data_types/m03_3_1_simple_data_types/number_parcing.js
@@ -0,0 +1,14 @@
+const strNum = "123";
+const strStr = "abc";
+
+const strNumNumber = Number(strNum);
+const strNumPlus = +strNum;
+
+const strStrNumber = Number(strStr);
+const strStrPlus = +strStr;
+
+console.log(typeof strNumNumber)
+console.log(typeof strNumPlus)
+
+console.log(strStrNumber);
+console.log(strStrPlus);
